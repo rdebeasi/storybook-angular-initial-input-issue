fbf457df05cc8726de2818e41ca69cafcb3912a0
Antoine Queru <antoine.queru@dataiku.com>
2022-01-25 16:28:38 +0100

    normal behavior with ng serve

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import GreeterComponent from 'src/stories/greeter.component';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GreeterComponent
   ],
   imports: [
     BrowserModule,
